7413182b64e4a23b3909ce4d20863b9f53fbe11b
J <jaycodes23@gmail.com>
2021-12-12 13:52:27 -0500

    create book class

---
+++ index.js
@@ -0,0 +1,14 @@
+class Book {
+  constructor(author, title, numPages, isCompleted) {
+    this.author = author
+    this.title = title
+    this.numPages = numPages
+    this.isCompleted = isCompleted
+  }
+}
+
+const books = [
+  new Book('JK Rowling', "Harry Potter and the Sorcerer's Stone", 307, true),
+  new Book('F. Scott Fitzgerald', 'The Great Gatsby', 212, true),
+  new Book('Alexandre Dumas', 'The Count of Monte Cristo', 1215, false)
+]
