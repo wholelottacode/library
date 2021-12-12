a5cd7e97925b8f620b9554b7e24b698c41eed20c
J <jaycodes23@gmail.com>
2021-12-12 15:08:38 -0500

    add function to make book nodes

--- index.js
+++ index.js
@@ -12,3 +12,23 @@ const books = [
   new Book('F. Scott Fitzgerald', 'The Great Gatsby', 212, true),
   new Book('Alexandre Dumas', 'The Count of Monte Cristo', 1215, false)
 ]
+
+const createBookElement = (book) => {
+  const listItem = document.createElement('li')
+  const bookElement = `
+    <div>
+      <h3>${book.title}</h3>
+      <p>${book.author}</p>
+      <p>Number of Pages: ${book.numPages}</p>
+      <input type='checkbox' ${book.isCompleted ? 'checked' : ''}/>
+    </div>
+  `
+  listItem.innerHTML = bookElement
+  return listItem
+}
+
+const booksList = document.querySelector('.books')
+books.forEach(book => {
+    const bookNode = createBookElement(book)
+    booksList.appendChild(bookNode)
+})
