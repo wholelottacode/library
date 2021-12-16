ca393d0eda34f355ee867d81a4d715f61e65fbf2
J <jaycodes23@gmail.com>
2021-12-15 20:03:29 -0500

    handle delete and checkbox clicks

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 class Book {
+  static count = 0
   constructor(title, author, numPages, isCompleted) {
     this.author = author
     this.title = title
     this.numPages = numPages
     this.isCompleted = isCompleted
+    this.id = Book.count
+    Book.count += 1
+  }
+
+  set completed(completed) {
+    this.isCompleted = completed
   }
 }
 
-const books = [
+let books = [
   new Book('JK Rowling', "Harry Potter and the Sorcerer's Stone", 307, true),
   new Book('F. Scott Fitzgerald', 'The Great Gatsby', 212, true),
   new Book('Alexandre Dumas', 'The Count of Monte Cristo', 1215, false)
 ]
 
 const createBookElement = (book) => {
-  const listItem = document.createElement('li')
-  const bookElement = `
-    <div>
+  const container = document.createElement('div')
+  const bookData = `
       <h3>${book.title}</h3>
       <p>${book.author}</p>
       <p>Number of Pages: ${book.numPages}</p>
-      <input type='checkbox' ${book.isCompleted ? 'checked' : ''}/>
-    </div>
   `
-  listItem.innerHTML = bookElement
-  return listItem
+  container.innerHTML = bookData
+
+  const checkbox = document.createElement('input')
+  checkbox.setAttribute('type', 'checkbox')
+  checkbox.setAttribute('id', book.id)
+  checkbox.checked = book.isCompleted
+  checkbox.addEventListener('click', handleCheckboxClick)
+  container.appendChild(checkbox)
+
+  const deleteButton = document.createElement('button')
+  deleteButton.setAttribute('id', book.id)
+  deleteButton.textContent = 'Delete'
+  deleteButton.addEventListener('click', handleDeleteClick)
+  container.appendChild(deleteButton)
+
+  const bookElement = document.createElement('li')
+  bookElement.appendChild(container)
+  return bookElement
 }
 
 const newBookButton = document.querySelector('button')
@@ -32,7 +52,6 @@ const form = document.querySelector('form')
 const booksList = document.querySelector('.books')
 
 const handleNewBookClick = (e) => {
-  console.log('show the form')
   e.target.classList.toggle('hide')
   form.classList.toggle('hide')
 }
@@ -65,7 +84,7 @@ const handleFormSubmit = (e) => {
   const inputs = Array.from(form.querySelectorAll("input:not(input[type='submit'])"))
   
   const [title, author, numPages, completed] = getFormData(inputs)
-  const book = new Book(title, author, numPages, completed)
+  const book = new Book(title, author, parseInt(numPages), completed)
   booksList.appendChild(createBookElement(book))
 
   clearForm(inputs)
@@ -73,14 +92,24 @@ const handleFormSubmit = (e) => {
   newBookButton.classList.toggle('hide')
 }
 
+const handleCheckboxClick = (e) => {
+  const checkbox = e.target
+  const book = books.find(book => book.id === Number(checkbox.getAttribute('id')))
+  book.completed = checkbox.checked
+}
+
+const handleDeleteClick = (e) => {
+  const deleteButton = e.target
+  books = books.filter(book => book.id !== Number(deleteButton.getAttribute('id')))
+  booksList.removeChild(deleteButton.parentNode.parentNode)
+}
+
 newBookButton.addEventListener('click', handleNewBookClick)
 form.addEventListener('submit', handleFormSubmit)
 form.classList.toggle('hide')
 
 books.forEach(book => {
-    const bookNode = createBookElement(book)
-    booksList.appendChild(bookNode)
+    booksList.appendChild(createBookElement(book))
 })
-
 
 
